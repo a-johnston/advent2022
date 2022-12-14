370a848f6af9cccec6aaba26b9e3df290ea3ddf5
Adam Johnston <amj69@case.edu>
2022-12-14 03:17:20 -0800

    several days

--- main.py
+++ main.py
@@ -7,25 +7,23 @@ import sys
 if __name__ == '__main__':
     day = sys.argv[1]
     mod = importlib.import_module(day + '.main')
-    if hasattr(mod, 'solve'):
-        solver = mod.solve
-        args = sys.argv[2:]
-    elif all((
-        len(sys.argv) > 2,
-        sys.argv[-1].startswith('p'),
-        hasattr(mod, f'solve_{sys.argv[-1]}'),
-    )):
-        solver = getattr(mod, f'solve_{sys.argv[-1]}')
-        args = sys.argv[2:-1]
-    else:
-        print(f'bad module {day}')
+    solvers = {}
+    for name in dir(mod):
+        if name.startswith('solve'):
+            solver = getattr(mod, name)
+            name = name[6:].replace('p', 'Part ')
+            solvers[name] = solver
+    if not solvers:
+        print(f'bad module {mod.__name__}')
         sys.exit(1)
 
     print(' '.join(sys.argv[1:]))
-    for file in os.listdir(day):
-        if not file.endswith('.txt'):
-            continue
-        print(f'\n{file}:')
-        with open(os.path.join(day, file)) as fp:
-            solution = solver((l.rstrip('\n') for l in fp.readlines()), *args)
-            print(f' {solution}')
+    for name, solver in solvers.items():
+        if name:
+            print('\n' + name)
+        for file in os.listdir(day):
+            if not file.endswith('.txt'):
+                continue
+            with open(os.path.join(day, file)) as fp:
+                solution = solver((l.rstrip('\n') for l in fp.readlines()), *sys.argv[2:])
+                print(f'{file:<12}: {solution}')
