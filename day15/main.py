81c000771f39461745aba0aa1f3cc1f239787adc
Adam Johnston <amj69@case.edu>
2022-12-18 21:01:54 -0800

    finally got this working!

--- day15/main.py
+++ day15/main.py
@@ -58,68 +58,8 @@ def solve_p1(lines):
     return sum(b - a for a, b in _merge(intervals))
 
 
-def _radius(cx, cy, r):
-    yield cx - r, cy
-    yield cx + r, cy
-    for x in range(cx - r + 1, cx + r):
-        d = r - cx + x
-        # yield x, cy - d
-        yield x, cy + d
-    for x in range(cx + r + 1, cx - r, -1):
-        d = r - cx + x
-        yield x, cy - d
-
-
-def _funky_sensor_sort(sensors):
-    # Sorts sensors based on minimum distance to non-adjacent sensors. This is
-    # slightly problem specific as we know there must be one solution, ergo it
-    # must be between two nodes without any gap, but this still works in the
-    # general case of finding unknown locations given sensor data. In addition
-    # this method returns a set of sensor indices to check for each sensor.
-    # That set is used for later processing based on the same edge-solution
-    # finding assumptions.
-    nn = {}
-    check_map = {}
-    for sa in sensors:
-        ax, ay, ar = sa
-        if sa not in check_map:
-            check_map[sa] = set()
-        for sb in sensors:
-            if sa == sb:
-                continue
-            bx, by, br = sb
-            d = _dist(ax, ay, bx, by)
-            if d < ar + br + 20:
-                check_map[sa].add(sb)
-            if d <= ar + br:
-                continue
-            if sa not in nn or nn[sa][1] > d:
-                nn[sa] = (sb, d, d - ar - br)
-
-    sensors = [(*s, list(check_map[s])) for s in sensors]
-    return sorted(sensors, key=lambda x: nn[x[:-1]][-1])
-
-
-def solve_p2(lines):
-    w, sensors = _parse_lines_to_header_and_sensors(lines, 2)
-    sensors = _funky_sensor_sort(sensors)
-
-    def valid(x, y, checks):
-        if x <= 0 or y <= 0 or x > w or y > w:
-            return False
-        for idx, (sx, sy, sr) in enumerate(checks):
-            if _dist(x, y, sx, sy) <= sr:
-                if idx != 0:
-                    check = checks.pop(idx)
-                    checks.insert(0, check)
-                return False
-        return True
-
-    for sensor in sensors:
-        sx, sy, sr, checks = sensor
-        for x, y in _radius(sx, sy, sr + 1):
-            if valid(x, y, checks):
-                return x * 4000000 + y
+def _out_of_bounds(x, y, w):
+    return x < 0 or y < 0 or x > w or y > w
 
 
 def _move(x, y, m, w):
@@ -128,30 +68,34 @@ def _move(x, y, m, w):
     if y < 0:
         return x - y * m, 0
     if x > w:
-        return w, y + (x - w) * m
+        return w, y - (x - w) * m
     if y > w:
-        return x + (y - w) * m, w
+        return x - (y - w) * m, w
     return x, y
 
 
 def _trim(x1, y1, x2, y2, m, w):
-    return *_move(x1, y1, m, w), *_move(x2, y2, -m, w), m
+    x1, y1 = _move(x1, y1, m, w)
+    x2, y2 = _move(x2, y2, m, w)
+    if _out_of_bounds(x1, y1, w) or _out_of_bounds(x2, y2, w):
+        return None
+    return x1, y1, x2, y2, m
 
 
 def _len(segment):
-    return abs(segment[0] - segment[2])
+    return abs(segment[0] - segment[2]) + 1
 
 
 def _get_intervals(x, y, r, w):
     # Generates non-overlapping line segments (x1, y1, x2, y2, m) where x1 < x2
     # and m is the slope; either -1 or 1. Each segment is 1 block outside of the
     # given sensor's radius.
-    return (
-        _trim(x - r - 1, y, x - 1, y - r, -1, w),
-        _trim(x, y - r - 1, x + r, y - 1, 1, w),
-        _trim(x + 1, y + r, x + r + 1, y, -1, w),
-        _trim(x - r, y - 1, x, y + r + 1, 1, w),
-    )
+    return tuple(filter(None, (
+        _trim(x - r, y, x - 1, y - r + 1, -1, w),
+        _trim(x, y - r, x + r - 1, y - 1, 1, w),
+        _trim(x + 1, y + r - 1, x + r, y, -1, w),
+        _trim(x - r + 1, y + 1, x, y + r, 1, w),
+    )))
 
 
 def _intersect(l, s):
@@ -162,10 +106,12 @@ def _intersect(l, s):
         return []
     # clips head
     if da >= 0:
-        return [(l[0] + da, l[1] + da * l[4], l[2], l[3], l[4])]
+        d = int((-db - 1) / 2)
+        return [(l[2] - d, l[3] - d * l[4], l[2], l[3], l[4])]
     # clips tail
     if db >= 0:
-        return [(l[0], l[1], l[2] - db, l[3] - db * l[4], l[4])]
+        d = int((-da - 1) / 2)
+        return [(l[0], l[1], l[0] + d, l[1] + d * l[4], l[4])]
     if l[0] < s[0] and s[0] < l[2] and min(l[1], l[3]) < s[1] and s[1] < max(l[1], l[3]):
         """ x1 = s[0]
             y1 = l[1] + (s[0] - l[0]) * l[4]
@@ -192,11 +138,11 @@ def _intersect(l, s):
     return [l]
 
 
-def solve_p3(lines):
+def solve_p2(lines):
     w, sensors = _parse_lines_to_header_and_sensors(lines, 2)
     all_intervals = []
     for i, (x, y, r) in enumerate(sensors):
-        all_intervals.append((i, _get_intervals(x, y, r, w)))
+        all_intervals.append((i, _get_intervals(x, y, r + 1, w)))
     for i, intervals in all_intervals:
         for interval in intervals:
             subintervals = [interval]
@@ -210,3 +156,24 @@ def solve_p3(lines):
             for interval in subintervals:
                 if _len(interval) > 0:
                     return interval[0] * 4000000 + interval[1]
+
+
+def test_add_interval(grid, interval, c):
+    x, y = interval[0], interval[1]
+    while x <= interval[2]:
+        test_mark_grid(grid, x, y, c)
+        x += 1
+        y += interval[4]
+
+
+def test_mark_grid(grid, x, y, c):
+    try:
+        grid[y][x] = c
+    except:
+        print(x, y, c)
+
+
+def test_print_grid(grid):
+    print('   ' + ''.join(str(x % 10) if x % 5 == 0 else ' ' for x in range(len(grid[0]))))
+    for y in range(len(grid)):
+        print(f'{y:<3}' + ''.join(grid[y]))
