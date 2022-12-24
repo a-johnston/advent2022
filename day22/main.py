92c8b667d5ca433300f9f11b08063bff220813ac
Adam Johnston <amj69@case.edu>
2022-12-23 20:45:29 -0800

    day 22 mucking

--- day22/main.py
+++ day22/main.py
@@ -87,11 +87,11 @@ def _get_w(grid):
 def _get_faces(grid):
     faces = {}
     w = _get_w(grid)
-    print(len(grid[0]), w)
     y = 0
     for x in range(0, len(grid[0]), w):
         if grid[y][x] != ' ':
             faces[0] = (x, y, 0)
+            break
 
     edge = {0}
     seen = set()
@@ -108,22 +108,76 @@ def _get_faces(grid):
                 continue
             if grid[ny][nx] == ' ':
                 continue
-            new_id = _face_edges[n][(d + nd) % len(dirs)]
+            edge_id = (nd - d) % len(dirs)
+            new_id = _face_edges[n][edge_id]
             if new_id not in seen:
-                new_d = (_face_edges[new_id].index(n) + nd + 2) % len(dirs)
+                new_d = (_face_edges[new_id].index(n) - nd + 2 + d) % len(dirs)
                 faces[new_id] = (nx, ny, new_d)
                 edge.add(new_id)
-    return faces
+    return w, faces
 
 
 class GridP2(Grid):
     def __init__(self, grid):
         super().__init__(grid)
-        self.faces = _get_faces(grid)
-        print(self.faces)
+        self.w, self.faces = _get_faces(grid)
+        print(self.w, len(self.faces), self.faces)
+
+    def _get_face(self, x, y):
+        for f, (fx, fy, d) in self.faces.items():
+            if x >= fx and x < fx + self.w and y >= fy and y < fy + self.w:
+                return f
+        return -1
+
+    def _encode_as_edge(self, f, d, x, y):
+        x -= self.faces[f][0]
+        y -= self.faces[f][1]
+        if d == 0:
+            assert y == 0
+            return x
+        elif d == 1:
+            assert x == self.w - 1
+            return y
+        elif d == 2:
+            assert y == self.w - 1
+            return self.w - x - 1
+        elif d == 3:
+            assert x == 0
+            return self.w - y - 1
+        raise ValueError(f'Not on edge {f=}, {d=}, {x=}, {y=}')
+
+    def _decode_from_edge(self, f, d, v):
+        fx, fy, fd = self.faces[f]
+        d = (d - self.faces[f][2]) % len(dirs)
+        if d == 0:
+            return fx + v, fy
+        if d == 1:
+            return fx + self.w  - 1, fy + v
+        if d == 2:
+            return fx + self.w - v - 1, fy + self.w - 1
+        if d == 3:
+            return fx, fy + self.w - v - 1
+        raise ValueError(f'Bad dir {d}')
 
     def move(self, x, y, d):
-        return x, y, d, False
+        dx, dy = dirs[d]
+        f = self._get_face(x, y)
+        if f == -1:
+            raise ValueError(f'Bad move start {x} {y}')
+        nx, ny = x + dx, y + dy
+        nf = self._get_face(nx, ny)
+        nd = d
+        if nf == -1 or self.grid[ny][nx] == ' ':
+            nf = _face_edges[f][d]
+            fd = _face_edges[nf].index(f)
+            ev = self._encode_as_edge(f, d, x, y)
+            nx, ny = self._decode_from_edge(nf, fd, self.w - ev - 1)
+            nd = ((d - self.faces[f][2]) + self.faces[nf][2]) % len(dirs)
+        #elif f != nf and _face_edges[f][d] != nf:
+        #    raise ValueError(f'Got to {nf} from {f} with move {d}')
+        if self.grid[ny][nx] == '#':
+            return x, y, d, True
+        return nx, ny, nd, False
 
 
 def _count(grid, x, y):
@@ -143,15 +197,14 @@ def sim_grid(grid, moves):
                 x, y, d, hit = grid.move(x, y, d)
                 if hit:
                     break
-    print(x, y)
-    return 1000 * (y + 1) + 4 * (x + 1) + (d - 1) % len(dirs)
+    d = (d - 1) % len(dirs)
+    print(x, y, d)
+    return 1000 * (y + 1) + 4 * (x + 1) + d
 
 
 def solve_p1(lines):
-    grid, moves = GridP1.parse(lines)
-    return sim_grid(grid, moves)
+    return sim_grid(*GridP1.parse(lines))
 
 
 def solve_p2(lines):
-    grid, moves = GridP2.parse(lines)
-    return sim_grid(grid, moves)
+    return sim_grid(*GridP2.parse(lines))
